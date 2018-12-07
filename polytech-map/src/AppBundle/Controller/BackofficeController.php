813aa069ba325fc25831cc8bff30b924a1219644
Epulapp <Epulapp@epulapp124.univ-lyon1.fr>
2018-12-07 09:34:35 +0100

    feat(login): ajout de l'écran de connexion

--- polytech-map/src/AppBundle/Controller/BackofficeController.php
+++ polytech-map/src/AppBundle/Controller/BackofficeController.php
@@ -18,4 +18,18 @@ class BackofficeController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/backoffice/login", name="backoffice_login")
+     */
+    public function loginAction(Request $request,$error ,$lastUsername)
+    {
+        // replace this example code with whatever you need
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+
+
 }
