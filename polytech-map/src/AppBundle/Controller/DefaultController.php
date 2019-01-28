933300d26184c4d0780ff85eaa90c40908c2850e
FlynnTheInfinite <fabienmorival@gmail.com>
2019-01-28 14:31:41 +0100

    Corrected JSON request

--- polytech-map/src/AppBundle/Controller/DefaultController.php
+++ polytech-map/src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\User;
 use AppBundle\Entity\Gare;
