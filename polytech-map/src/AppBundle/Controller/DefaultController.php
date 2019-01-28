1a46abe8f17c1ad8c8280c99ace3092916365ba3
Epulapp <Epulapp@epulapp124.univ-lyon1.fr>
2019-01-28 15:20:54 +0100

    fix(ajax): Récupération du geojson des gares

--- polytech-map/src/AppBundle/Controller/DefaultController.php
+++ polytech-map/src/AppBundle/Controller/DefaultController.php
@@ -13,39 +13,37 @@ use AppBundle\Entity\Ecole;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+         * @Route("/", name="homepage")
+         */
+        public function indexAction(Request $request)
+        {
+            // replace this example code with whatever you need
+            return $this->render('default/index.html.twig', [
+                'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            ]);
+        }
+        /**
+       * @Route("/ajax", name="homepage_ajax")
+    */
+    public function ajaxAction(Request $request) {
+       $students = $this->getDoctrine()
+          ->getRepository('AppBundle:Gare')
+          ->findAll();
+
+          $gareRepository = $this->getDoctrine()->getRepository(Gare::class);
+
+          $pollutionRepository = $this->getDoctrine()->getRepository(Pollution::class);
+          $ecoleRepository = $this->getDoctrine()->getRepository(Ecole::class);
+
+
+          $data = json_decode($request->getContent(), true);
+
+           if ($request->isXmlHttpRequest()) {
+              $jsonData = array();
+              $idx = 0;
+              return new JsonResponse($gareRepository->genererGeoJSON($data['latlng']['lat'], $data['latlng']['lng'], 10));
+           } else {
+              return $this->render('default/index.html.twig');
+           }
     }
-    /**
-   * @Route("/ajax", name="homepage_ajax")
-*/
-public function ajaxAction(Request $request) {
-   $students = $this->getDoctrine()
-      ->getRepository('AppBundle:Gare')
-      ->findAll();
-
-      $gareRepository = $this->getDoctrine()->getRepository(Gare::class);
-
-      $pollutionRepository = $this->getDoctrine()->getRepository(Pollution::class);
-      $ecoleRepository = $this->getDoctrine()->getRepository(Ecole::class);
-
-      $data = json_decode($request->getContent(), true);
-
-        dump($data);
-        return $data;
- //  if ($request->isXmlHttpRequest() || $request->query->get('showJson') == 1) {
-   if ($request->isXmlHttpRequest()) {
-      $jsonData = array();
-      $idx = 0;
-      return new JsonResponse($gareRepository->genererGeoJSON(45.5391, 6, 10));
-   } else {
-      return $this->render('default/index.html.twig');
-   }
-}
 }
